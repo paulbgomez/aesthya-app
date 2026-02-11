5b0f0ab920ce5ae6031710146c5b944f687347fa
Ratbwoi <paulbg1993@gmail.com>
2026-02-11 17:11:18 +0100

    Add soundcloud api

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,46 @@
 use App\Http\Controllers\Feelings\FeelingsController;
 use App\Http\Controllers\Feelings\GenerateContentController;
 use App\Http\Controllers\Moodboard\MoodboardController;
+use App\Services\Music\SoundCloudService;
 use App\Services\Poem\PoetryDBService;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/test', function () {
-    return (new PoetryDBService)->fetchPoemByAuthorAndTitle('Blake', 'Tree');
+    $trackData = (new SoundCloudService)->searchTrackByTitle('nuevayol');
+    
+    if (!$trackData || !$trackData['stream_url']) {
+        return 'No stream URL found';
+    }
+    
+    $streamUrl = $trackData['stream_url'];
+    $souncloudUrl = $trackData['soundcloud_url'];
+    
+    return <<<HTML
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Stream Test</title>
+</head>
+<body>
+    <h2>{$trackData['title']}</h2>
+    <p><strong>Stream URL:</strong> {$streamUrl}</p>
+    <p><strong>SoundCloud URL:</strong> {$souncloudUrl}</p>
+    
+    <iframe 
+        width="100%" 
+        height="166" 
+        scrolling="no" 
+        frameborder="no"
+        src="https://w.soundcloud.com/player/?url={$souncloudUrl}&auto_play=true&hide_related=true&show_comments=false"
+    ></iframe>
+    
+    <hr>
+    <h3>Full Track Data:</h3>
+</body>
+</html>
+HTML;
 });
 
 Route::get('/', function () {
